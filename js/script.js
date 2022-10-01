7192ee27fafcb2f1bb84fc0f552055f6bcacd333
loWOLFol <98999198+loWOLFol@users.noreply.github.com>
2022-10-01 21:30:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    if (error === 0) {
+      function submit() {
+        let inputs = document.querySelectorAll('input');
+        let input_values = [];
+        for (i = 0; i < inputs.length; i++) {
+          input_values.push(inputs[i].value);
+
+        }
+        console.log(input_values);
+
+      }
+      let button = document.querySelector('.btn');
+      button.addEventListener('click', submit);
+
+    } else {
+      alert('Заполните обязательные поля!');
+    }
+  }
+
+  function formValidate(form) {
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+
+    for (let i = 0; i < formReq.length; i++) {
+      const input = formReq[i];
+      formRemoveError(input);
+
+      if (input.classList.contains('_email')) {
+        if (emailTest(input)) {
+          formAddError(input);
+          error++;
+        }
+      } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+        formAddError(input);
+        error++;
+      } else {
+        if (input.value === '') {
+          formAddError(input);
+          error++;
+        }
+      }
+
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+
+  function formRemoveError(input) {
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+
+  function emailTest(input) {
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+  }
+});
+
